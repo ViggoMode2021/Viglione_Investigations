e1df571718920231f1cd30d52f248c2d23741761
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-06-07 18:37:54 -0400

    Add files via upload

--- urls.py
+++ urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('testimonials/', views.testimonials, name='blog-testimonials'),
+    path('services/', views.services, name='blog-services'),
+    path('references/', views.references, name='blog-references'),
+    path('pay_invoice/', views.pay_invoice, name='blog-pay_invoice'),
 ]
--- views.py
+++ views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'blog/home.html')
 
-
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+def testimonials(request):
+    return render(request, 'blog/testimonials.html', {'title': 'Testimonials'})
+
+def services(request):
+    return render(request, 'blog/services.html', {'title': 'Services'})
+
+def references(request):
+    return render(request, 'blog/references.html', {'title': 'References'})
+
+def pay_invoice(request):
+    return render(request, 'blog/pay_invoice.html', {'title': 'Pay invoice'})
