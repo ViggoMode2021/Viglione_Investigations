0bd9c1922f6a8c17eebe22c60b8cc70faba7c89a
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-06-18 12:03:52 -0400

    Add files via upload

--- urls.py
+++ urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('testimonials/', views.testimonials, name='blog-testimonials'),
-    path('services/', views.services, name='blog-services'),
     path('references/', views.references, name='blog-references'),
     path('home_spanish/', views.home_spanish, name='blog-home_spanish'),
     path('testimonials_spanish/', views.testimonials_spanish, name='blog-testimonials_spanish'),
--- views.py
+++ views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 
+# English pages
+
 def home(request):
     return render(request, 'blog/home.html')
 
 def testimonials(request):
     return render(request, 'blog/testimonials.html', {'title': 'Testimonials'})
 
-def services(request):
-    return render(request, 'blog/services.html', {'title': 'Services'})
-
 def references(request):
     return render(request, 'blog/references.html', {'title': 'References'})
 
-#Spanish pages
+# Spanish pages
+
 def home_spanish(request):
     return render(request, 'blog/home_spanish.html', {'title': 'Home - Spanish'})
 
